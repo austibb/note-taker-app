8f90545adf73beda2eb1531f3e40e548b8e0860d
Austin Wu <austinwu@Austins-Mac-mini.local>
2022-07-26 23:08:29 -0400

    cleaning files and resetting db.json

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 // set up express app to handle dataparsing
 app.use(express.json());
-// app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, "./public")));
-// app.use(routes);
 
 
 // serves home page
@@ -24,9 +22,9 @@ app.get('/notes', (req, res) => {
 });
 
 // Catches all to send to home page
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'))
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
 // creates the bridge to api and serves the db.json when api/notes is entered in url
 app.get('/api/notes', (req, res) => {
@@ -41,6 +39,7 @@ app.post('/api/notes', (req, res) => {
     fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
 });
 
+// deletes the selected note, searching via ID
 app.delete("/api/notes/:id", function(req, res) {
     let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     let noteID = req.params.id;
